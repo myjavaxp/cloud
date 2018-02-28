c690116976000fe7ed9bc4626d5ef6c79455400e
XuYibo <xuyibo@anji-plus.com>
2018-02-28 10:30:43 +0800

    消费者新增Controller

--- ribbonconsumer/src/main/java/com/yibo/ribbonconsumer/controller/ConsumerController.java
+++ ribbonconsumer/src/main/java/com/yibo/ribbonconsumer/controller/ConsumerController.java
@@ -3,8 +3,11 @@ package com.yibo.ribbonconsumer.controller;
 import com.yibo.ribbonconsumer.entity.Mission;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.annotation.Resource;
+import java.net.URI;
 
 @RestController
 public class ConsumerController {
@@ -20,8 +23,19 @@ public class ConsumerController {
     public String addMission(@RequestBody Mission mission) {
         return restTemplate.postForEntity("http://HELLO-SERVICE/mission/post", mission, String.class).getBody();
     }
-    @RequestMapping(value = "/getMission",method = RequestMethod.GET)
-    public String getMission(@RequestParam("id") Long id){
-        return restTemplate.getForEntity("http://HELLO-SERVICE/mission/get?id={1}",String.class,id).getBody();
+
+    @RequestMapping(value = "/getMission", method = RequestMethod.GET)
+    public String getMission(@RequestParam("id") Long id) {
+        return restTemplate.getForEntity("http://HELLO-SERVICE/mission/get?id={1}", String.class, id).getBody();
+    }
+
+    @RequestMapping(value = "/getMyMission", method = RequestMethod.GET)
+    public String getMyMission(@RequestParam("id") Long id) {
+        UriComponents uriComponents = UriComponentsBuilder.fromUriString("http://HELLO-SERVICE/mission/get?id={id}")
+                .build()
+                .expand(id)
+                .encode();
+        URI uri = uriComponents.toUri();
+        return restTemplate.getForEntity(uri, String.class).getBody();
     }
 }
