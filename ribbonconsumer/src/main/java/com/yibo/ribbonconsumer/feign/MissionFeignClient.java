0adc8d51515bb6d77fb5b608036cacfe32bb609d
Yibo <xuyibo@outlook.com>
2018-03-12 18:45:58 +0800

    排除不需要的包

--- ribbonconsumer/src/main/java/com/yibo/ribbonconsumer/feign/MissionFeignClient.java
+++ ribbonconsumer/src/main/java/com/yibo/ribbonconsumer/feign/MissionFeignClient.java
@@ -2,15 +2,13 @@ package com.yibo.ribbonconsumer.feign;
 
 import com.yibo.ribbonconsumer.component.FeignClientFallBack;
 import com.yibo.springboothello.entity.Mission;
-import feign.Param;
-import feign.RequestLine;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "hello-service",fallback = FeignClientFallBack.class)
+@FeignClient(name = "hello-service", fallback = FeignClientFallBack.class)
 public interface MissionFeignClient {
-    @RequestMapping(value = "/{id}",method = RequestMethod.GET)
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     Mission findById(@PathVariable("id") Long id);
 }
