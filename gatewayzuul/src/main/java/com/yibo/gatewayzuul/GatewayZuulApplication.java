3ad8eace858dee7d175fd8d6c41a056a985cc3aa
Yibo <xuyibo@outlook.com>
2018-03-13 19:13:36 +0800

    排除不需要的包

--- gatewayzuul/src/main/java/com/yibo/gatewayzuul/GatewayZuulApplication.java
+++ gatewayzuul/src/main/java/com/yibo/gatewayzuul/GatewayZuulApplication.java
@@ -2,10 +2,12 @@ package com.yibo.gatewayzuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableHystrix
 public class GatewayZuulApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayZuulApplication.class,args);
---
+++ gatewayzuul/src/main/java/com/yibo/gatewayzuul/filter/PreRequestLogFilter.java
@@ -0,0 +1,25 @@
+package com.yibo.gatewayzuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+
+public class PreRequestLogFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        return null;
+    }
+}
