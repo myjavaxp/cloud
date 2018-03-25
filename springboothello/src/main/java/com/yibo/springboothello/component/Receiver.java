c294270b9cf440b7e48b98dcea3c81f4e0f5c751
Yibo <xuyibo@outlook.com>
2018-03-25 11:07:52 +0800

    RabbitMQ demo

--- springboothello/src/main/java/com/yibo/springboothello/component/Receiver.java
+++ springboothello/src/main/java/com/yibo/springboothello/component/Receiver.java
@@ -1,13 +1,16 @@
 package com.yibo.springboothello.component;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 @Component
 @RabbitListener(queues = "hello")
 public class Receiver {
+    private static final Logger LOGGER= LoggerFactory.getLogger(Receiver.class);
     @RabbitHandler
     public void process(String hello){
-        System.out.println("Receiver: "+hello);
+        LOGGER.info("Receiver: "+hello);
     }
 }
