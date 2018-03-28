ffed1166ba2bea3963544596ccbe7a87a1621bb6
XuYibo <xuyibo@anji-plus.com>
2018-03-28 13:38:34 +0800

    链路追踪改造

--- zipkinserver/src/main/java/com/yibo/zipkinserver/ZipkinServerApplication.java
+++ zipkinserver/src/main/java/com/yibo/zipkinserver/ZipkinServerApplication.java
@@ -2,12 +2,12 @@ package com.yibo.zipkinserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import zipkin.server.EnableZipkinServer;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 
-@EnableZipkinServer
+@EnableZipkinStreamServer
 @SpringBootApplication
 public class ZipkinServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ZipkinServerApplication.class,args);
+        SpringApplication.run(ZipkinServerApplication.class, args);
     }
 }
