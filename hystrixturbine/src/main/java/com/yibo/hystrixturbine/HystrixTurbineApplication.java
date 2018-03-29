eab89590b0cbe2a2530da678f9e6882a890723ba
XuYibo <xuyibo@anji-plus.com>
2018-03-29 09:07:12 +0800

    Hystrix监控使用turbine，使用消息队列rabbitMQ

--- hystrixturbine/src/main/java/com/yibo/hystrixturbine/HystrixTurbineApplication.java
+++ hystrixturbine/src/main/java/com/yibo/hystrixturbine/HystrixTurbineApplication.java
@@ -3,13 +3,13 @@ package com.yibo.hystrixturbine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 
 @SpringBootApplication
-@EnableTurbine
+@EnableTurbineStream
 @EnableDiscoveryClient
 public class HystrixTurbineApplication {
     public static void main(String[] args) {
-        SpringApplication.run(HystrixTurbineApplication.class,args);
+        SpringApplication.run(HystrixTurbineApplication.class, args);
     }
 }
