f73bd8348d172f4f41b5bc5a325ddf7c7bec6362
XuYibo <xuyibo@anji-plus.com>
2018-04-03 13:21:38 +0800

    移除ELK日志系统

--- serviceprovider/src/main/java/com/yibo/serviceprovider/controller/DealerController.java
+++ serviceprovider/src/main/java/com/yibo/serviceprovider/controller/DealerController.java
@@ -4,7 +4,6 @@ import com.yibo.serviceprovider.dao.DealerRepository;
 import com.yibo.serviceprovider.entity.Dealer;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,7 +25,6 @@ public class DealerController {
 
     @GetMapping("/{page}/{size}")
     public Page<Dealer> getDealersInPage(@PathVariable("page") Integer page, @PathVariable("size") Integer size) {
-        Assert.isTrue(page > -1 && size > 0, "页码信息不正确！");
         return dealerRepository.findAll(new PageRequest(page, size));
     }
 }
