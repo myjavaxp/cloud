d788e3d2743b30226448e5610124cce592a2c302
XuYibo <xuyibo@anji-plus.com>
2018-04-13 13:06:53 +0800

    代码优化

--- ribbonconsumer/src/main/java/com/yibo/ribbonconsumer/feign/MissionFeignClient.java
+++ ribbonconsumer/src/main/java/com/yibo/ribbonconsumer/feign/MissionFeignClient.java
@@ -3,12 +3,11 @@ package com.yibo.ribbonconsumer.feign;
 import com.yibo.ribbonconsumer.component.FeignClientFallBack;
 import com.yibo.springboothello.entity.Mission;
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @FeignClient(name = "hello-service", fallback = FeignClientFallBack.class)
 public interface MissionFeignClient {
-    @RequestMapping(value = "/mission/{id}", method = RequestMethod.GET)
+    @GetMapping("/mission/{id}")
     Mission findById(@PathVariable("id") Long id);
 }
--- ribbonconsumer/src/main/java/com/yibo/ribbonconsumer/feign/UserFeignClient.java
+++ ribbonconsumer/src/main/java/com/yibo/ribbonconsumer/feign/UserFeignClient.java
@@ -3,12 +3,11 @@ package com.yibo.ribbonconsumer.feign;
 import com.yibo.ribbonconsumer.component.FeignClientFallbackFactory;
 import com.yibo.springboothello.entity.Mission;
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @FeignClient(name = "hello-service", fallbackFactory = FeignClientFallbackFactory.class)
 public interface UserFeignClient {
-    @RequestMapping(value = "/mission/{id}", method = RequestMethod.GET)
+    @GetMapping("/mission/{id}")
     Mission findById(@PathVariable("id") Long id);
 }
