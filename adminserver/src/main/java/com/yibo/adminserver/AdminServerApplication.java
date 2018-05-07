c7bd8c08aabcf3877866d46ecf47ce71248782a0
XuYibo <xuyibo@anji-plus.com>
2018-05-07 09:44:57 +0800

    增加admin监控模块

--- adminserver/src/main/java/com/yibo/adminserver/AdminServerApplication.java
+++ adminserver/src/main/java/com/yibo/adminserver/AdminServerApplication.java
@@ -1,15 +1,15 @@
-package com.yibo.hystrixturbine;
+package com.yibo.adminserver;
 
+import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 
 @SpringBootApplication
-@EnableTurbineStream
 @EnableEurekaClient
-public class HystrixTurbineApplication {
+@EnableAdminServer
+public class AdminServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(HystrixTurbineApplication.class, args);
+        SpringApplication.run(AdminServerApplication.class, args);
     }
 }
--- springboothello/src/main/java/com/yibo/springboothello/SpringBootHelloApplication.java
+++ springboothello/src/main/java/com/yibo/springboothello/SpringBootHelloApplication.java
@@ -3,11 +3,11 @@ package com.yibo.springboothello;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 @EnableTransactionManagement
 @MapperScan("com.yibo.springboothello.dao")
 public class SpringBootHelloApplication {
